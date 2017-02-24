1229c08a773748cd2c55377f4783cd6e8de3c41d
Wayne <wajanik@gmail.com>
2017-02-24 01:02:42 +0000

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -9,6 +9,9 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.1'
 
+# Bootstrap GEM
+gem 'bootstrap-sass', '3.3.6'
+
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   
   
+
   root 'pages#home'
   
-  get 'help', to: 'pages#help'
-  get 'about', to: 'pages#about'
+  get '/home', to: 'pages#home'
+  get '/help', to: 'pages#help'
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
+  
+  get  '/signup',  to: 'users#new'
   
 
 
