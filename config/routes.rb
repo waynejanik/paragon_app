fc8b861d56b34d9b1a20a28807c83fb850ef7955
Wayne <wajanik@gmail.com>
2017-03-13 18:23:33 +0000

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   
   
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   get 'users/show'
@@ -25,6 +29,8 @@ Rails.application.routes.draw do
   resources :users
   
   resources :account_activations, only: [:edit]
+  
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303194031) do
+ActiveRecord::Schema.define(version: 20170313174951) do
 
   create_table "users", force: :cascade do |t|
     t.string   "firstName"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 20170303194031) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
 end
